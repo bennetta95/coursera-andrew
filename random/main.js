e9392380f68aab265f011471aacff0d0dcd0d349
bennetta95 <63069429+bennetta95@users.noreply.github.com>
2020-04-16 01:33:10 +0300

    Add files via upload

---
+++ random/main.js
@@ -0,0 +1,51 @@
+
+function myFunction(){
+      document.getElementById("round2").style.display="unset";
+    }
+
+
+function myFunction2(){
+      document.getElementById("round3").style.display="unset";
+    }
+
+function myFunction3(){
+      document.getElementById("round4").style.display="unset";
+    }
+
+function myFunction4(){
+      document.getElementById("round5").style.display="unset";
+    }
+
+function myFunction5(){
+      document.getElementById("round6").style.display="unset";
+    }
+
+
+function myFunction6(){
+      alert("You have completed the Quiz!!!!");
+    }
+
+function myFunctionAns1(){
+	  window.open("AnswersSection1.pdf", "_blank");
+
+}
+
+function myFunctionAns2(){
+	  window.open("AnswersSection2 .pdf", "_blank");
+
+}
+
+function myFunctionAns3(){
+	  window.open("AnswersSection3-4.pdf", "_blank");
+
+}
+
+function myFunctionAns4(){
+	  window.open("AnswersSection5.pdf", "_blank");
+
+}
+
+function myFunctionAns5(){
+	  window.open("AnswersFinale.pdf", "_blank");
+
+}
